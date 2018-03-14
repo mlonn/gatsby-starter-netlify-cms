965f886a748088fe2cfa00253b44983549d2fbe7
Mikael Lönn <mikael.lonn@outlook.com>
2018-03-14 15:33:49 +0100

    added file

---
+++ src/components/DocumentCategories.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const DocumentCategories = ({ categories }) => (
+  <div className="columns is-multiline">
+    {categories.map(category => {
+      const cat = category.category;
+      return (
+        <div key={cat.categoryName}>
+          <section className="section">
+            <h3 className="title">{cat.categoryName}</h3>
+            {cat.files.map(file => <Link to={file.file}>{file.name}</Link>)}
+          </section>
+        </div>
+      );
+    })}
+  </div>
+);
+export default DocumentCategories;
