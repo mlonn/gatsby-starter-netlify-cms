1394d913eab3ab5a4a20bdc73f44f7d9a0ec83d4
Mikael Lönn <mikael.lonn@outlook.com>
2018-03-16 15:04:19 +0100

    Update DocumentCategories.js

--- src/components/DocumentCategories.js
+++ src/components/DocumentCategories.js
@@ -2,19 +2,17 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const DocumentCategories = ({ categories }) => (
-  <div className="columns is-multiline">
+  <div className="hero">
     {categories.map(category => {
       const cat = category.category;
       return (
         <div>
-          <div className="hero">
             <h3 className="title">{cat.categoryName}</h3>
           {cat.files.map(file => <Link to={file.file}>{file.name}</Link>)}
-          </div>
-          <hr/>
         </div>
       );
     })}
+    <hr/>
   </div>
 );
 export default DocumentCategories;
