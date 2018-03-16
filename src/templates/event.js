99bea6a39ac9bf3fabf492c67c389fcdba11e3ad
Mikael Lönn <mikael.lonn@outlook.com>
2018-03-16 14:15:50 +0100

    Update event.js

--- src/templates/event.js
+++ src/templates/event.js
@@ -12,7 +12,6 @@ export const EventTemplate = ({ content, contentComponent, description, title, h
         <div className="columns">
           <div className="column is-10 is-offset-1">
             <h1 className="title is-size-2 has-text-weight-bold is-bold-light">{title}</h1>
-            <p>{description}</p>
             <PostContent content={content} />
           </div>
         </div>
