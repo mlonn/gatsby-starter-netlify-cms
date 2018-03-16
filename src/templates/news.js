e03755151818c6a06ed06be417961b42c659517b
Mikael Lönn <mikael.lonn@outlook.com>
2018-03-16 14:16:07 +0100

    Update news.js

--- src/templates/news.js
+++ src/templates/news.js
@@ -12,7 +12,6 @@ export const NewsTemplate = ({ content, contentComponent, description, title, he
         <div className="columns">
           <div className="column is-10 is-offset-1">
             <h1 className="title is-size-2 has-text-weight-bold is-bold-light">{title}</h1>
-            <p>{description}</p>
             <PostContent content={content} />
           </div>
         </div>
@@ -43,7 +42,6 @@ export const pageQuery = graphql`
       frontmatter {
         date(formatString: "MMMM DD, YYYY")
         title
-        description
       }
     }
   }
