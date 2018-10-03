add686578c2097263d25226e7ee2ac2f1b1c84b6
Mikael Lönn <mikael.lonn@outlook.com>
2018-10-03 11:43:41 +0200

    Proper document list

--- src/components/DocumentCategories.js
+++ src/components/DocumentCategories.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const DocumentCategories = ({ categories }) => (
   <div className="hero">
@@ -7,13 +7,18 @@ const DocumentCategories = ({ categories }) => (
       const cat = category.category;
       return (
         <div>
-            <h3 className="title">{cat.categoryName}</h3>
-          {cat.files.map(file => <Link to={file.file}>{file.name}</Link>)}
-           <hr/>
+          <h3 className="title">{cat.categoryName}</h3>
+          <ul>
+            {cat.files.map(file => (
+              <li>
+                <Link to={file.file}>{file.name}</Link>
+              </li>
+            ))}
+          </ul>
+          <hr />
         </div>
       );
     })}
-    
   </div>
 );
 export default DocumentCategories;
